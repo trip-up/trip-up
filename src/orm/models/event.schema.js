3bfbe6ca40f1bdebddeb330e6d05f6c80895d56d
AstroKD <kdreyerinseattle@gmail.com>
2020-03-10 19:52:06 -0700

    event.schema

--- src/orm/models/event.schema.js
+++ src/orm/models/event.schema.js
@@ -1,10 +1,15 @@
+/**
+ * @requires Sequalize
+ * @module "role.schema"
+ * @description Export a function that can be called with a sequelize connection obj to instantiate the class Event, which is a Sequelize Model.
+ * <br> Add associate() prototype function, which can be called to add table associations. 
+ * @param {string} name - name of event
+ * @param {string} start_day - start_day of event
+ * @param {string} end_day - end_day of event
+ * @returns {object} Event
+ */
 const Sequelize = require('sequelize')
 
-/**
- * Export a function that can be called with a sequelize connection obj to instantiate the class Event, which is a Sequelize Model.
- * 
- * Add associate() prototype function, which can be called to add table associations. 
- */
 module.exports = function (sequelize) {
   class Event extends Sequelize.Model { }
 
