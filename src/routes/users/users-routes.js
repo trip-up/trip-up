fb06addac05218804e7476c7a6977ab548f31ad1
caitleigh <caitleigh27@gmail.com>
2020-03-11 13:21:49 -0700

    added comments for route format

--- src/routes/users/users-routes.js
+++ src/routes/users/users-routes.js
@@ -6,8 +6,11 @@ const bearerAuth = require('../../middleware/auth/bearer_auth')
 const {signUp, signIn, getAllUsers} = require('./user-route-handlers')
 
 authRouter.post('/signup', signUp)
+// format request: http :3000/signup name=*** email=**** password=*** city=**** phone****
 authRouter.post('/signin', basicAuth, signIn)
-authRouter.get('/user', bearerAuth, getAllUsers)
+//format request: http -a email:password post :3000/signin
+authRouter.get('/users', bearerAuth, getAllUsers)
+//format request: http get :3000/users 'Authoriation: Bearer TOKEN'
 
 
 module.exports = authRouter
