949a0b65116bcfed8a43b1b07ed1b766b6bed6fb
Tyler <tylersayvetz@gmail.com>
2020-03-11 14:16:32 -0700

    require bearer auth roles-routes

--- src/routes/roles/roles-route.js
+++ src/routes/roles/roles-route.js
@@ -8,26 +8,26 @@ const roleRouter = express.Router();
 const ACL = require('../../middleware/accessControlList')
 const CREDENTIALS = require('../../../config/serverSettings')
 
-const {addRole, getRole, updateRole, deleteRole} = require('./roles-route-handlers')
+const { addRole, getRole, updateRole, deleteRole } = require('./roles-route-handlers')
 
 /**
  * http post :3000/roles name=test42 create=true read=true update=true delete=false
  */
-roleRouter.post('/roles', ACL(CREDENTIALS.ADMIN), addRole)
+roleRouter.post('/roles', bearerAuth, ACL(CREDENTIALS.ADMIN), addRole)
 
 /**
  * http get :3000/roles
  */
-roleRouter.get('/roles', ACL(CREDENTIALS.ADMIN), getRole)
+roleRouter.get('/roles', bearerAuth, ACL(CREDENTIALS.ADMIN), getRole)
 
 /**
  * http put :3000/roles/42 name=test42 create=true read=true update=true delete=false
  */
-roleRouter.put('/roles/:id', ACL(CREDENTIALS.ADMIN), updateRole)
+roleRouter.put('/roles/:id', bearerAuth, ACL(CREDENTIALS.ADMIN), updateRole)
 
 /**
  * http delete :3000/roles/42
  */
-roleRouter.delete('/roles/:id', ACL(CREDENTIALS.ADMIN), deleteRole)
+roleRouter.delete('/roles/:id', bearerAuth, ACL(CREDENTIALS.ADMIN), deleteRole)
 
 module.exports = roleRouter
