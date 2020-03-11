554d98615857cdbfb3802c7b6101ff0c56cf615c
AstroKD <kdreyerinseattle@gmail.com>
2020-03-10 17:21:13 -0700

    get events by trip_ip working

--- src/routes/events/events-route-handlers.js
+++ src/routes/events/events-route-handlers.js
@@ -4,45 +4,45 @@
  * @description Callback functions for Event routes
  */
 const { Event } = require('../../orm/index')
- /**
-  * Add Event
-  * @param {*} req 
-  * @param {*} res 
-  * @param {*} next 
-  */
-  async function addEvent(req, res, next) {
-    console.log('req.body',req.body)
-    console.log('req.params',req.params)
-    await Event.create({
-          name: req.body.name,
-          start_day: req.body.start_day,
-          end_day: req.body.end_day,
-          trip_id: req.params.trip_id
-    })
-    .then(function (event, created) {
-      // console.log(event.get({
-      //     plain: true
-      // }))
-      if(created) console.log('created', created)
-      res.status(201).json(event)
-    })
-    .catch(next)
-  }
+
+/**
+* Add Event
+* @param {*} req 
+* @param {*} res 
+* @param {*} next 
+*/
+async function addEvent(req, res, next) {
+  await Event.create({
+        name: req.body.name,
+        start_day: req.body.start_day,
+        end_day: req.body.end_day,
+        trip_id: req.params.trip_id
+  })
+  .then(function (event, created) {
+    // console.log(event.get({
+    //     plain: true
+    // }))
+    if(created) console.log('created', created)
+    res.status(201).json(event)
+  })
+  .catch(next)
+}
 
 /**
  * Get Events from Trip
  * @param {*} req 
  * @param {*} res 
  * @param {*} next 
- * Not working: returns "error": "role is not defined"
  */
 async function getEventsFromTrip(req, res, next) {
+  console.log('req.body',req.body)
+  console.log('req.params',req.params)
   await Event.findAll({
     where: {trip_id: req.params.trip_id}
   })
   .then(function (result) {
     console.log('getEventfromTrip',result)
-    res.status(200).json(role)})
+    res.status(200).json(result)})
   .catch(next)
 }
 
